a6c164d27053d713a0ee350bcf827b6b1e7bdb05
Andej Nesterenko <mentatus.n@gmail.com>
2021-12-20 20:32:09 +0300

    урок 3 подключили простейший ViewPagerAdapter.kt

---
+++ app/src/main/java/com/gb/material_1507_1544_3_1/view/api/ApiActivity.kt
@@ -0,0 +1,18 @@
+package com.gb.material_1507_1544_3_1.view.api
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gb.material_1507_1555_3_1.databinding.ActivityApiBinding
+
+class ApiActivity:AppCompatActivity() {
+    lateinit var binding: ActivityApiBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityApiBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager)
+    }
+
+}
---
+++ app/src/main/java/com/gb/material_1507_1544_3_1/view/api/ViewPagerAdapter.kt
@@ -0,0 +1,26 @@
+package com.gb.material_1507_1544_3_1.view.api
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+private const val EARTH_FRAGMENT = 0
+private const val MARS_FRAGMENT = 1
+private const val SYSTEM_FRAGMENT = 2
+
+//TODO избавиться от устаревшего FragmentStatePagerAdapter
+class ViewPagerAdapter(private  val fm: FragmentManager): FragmentStatePagerAdapter(fm) {
+
+    private val fragments = arrayOf(EarthFragment(),MarsFragment(),SystemFragment())
+
+    override fun getCount()= fragments.size
+
+    override fun getItem(position: Int): Fragment {
+        return when(position){
+            EARTH_FRAGMENT ->fragments[EARTH_FRAGMENT]
+            MARS_FRAGMENT ->fragments[MARS_FRAGMENT ]
+            SYSTEM_FRAGMENT ->fragments[SYSTEM_FRAGMENT]
+            else -> fragments[EARTH_FRAGMENT]
+        }
+    }
+}
