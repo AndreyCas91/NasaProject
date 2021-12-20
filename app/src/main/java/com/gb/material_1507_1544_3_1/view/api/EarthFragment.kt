9ab65132c6186f73637bc381068938d80fedd839
Andej Nesterenko <mentatus.n@gmail.com>
2021-12-20 20:12:15 +0300

    урок 3 начали

---
+++ app/src/main/java/com/gb/material_1507_1544_3_1/view/api/EarthFragment.kt
@@ -0,0 +1,19 @@
+package com.gb.material_1507_1544_3_1.view.api
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.gb.material_1507_1555_3_1.R
+
+class EarthFragment:Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_earth,container,false)
+    }
+}
