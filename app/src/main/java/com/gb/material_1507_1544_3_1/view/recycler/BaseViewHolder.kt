6a8a50406e69ae0b04dedacfe2b2957a8a7e1f7c
Andej Nesterenko <mentatus.n@gmail.com>
2021-12-30 21:24:01 +0300

    урок 6 Добавили заголовок +рефакторинг (выделили у ViewHolder'ов предка BaseViewHolder.kt)

---
+++ app/src/main/java/com/gb/material_1507_1544_3_1/view/recycler/BaseViewHolder.kt
@@ -0,0 +1,8 @@
+package com.gb.material_1507_1544_3_1.view.recycler
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class BaseViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    abstract fun bind(data: Data)
+}
--- app/src/main/java/com/gb/material_1507_1544_3_1/view/recycler/Data.kt
+++ app/src/main/java/com/gb/material_1507_1544_3_1/view/recycler/Data.kt
@@ -3,4 +3,5 @@ package com.gb.material_1507_1544_3_1.view.recycler
 
 const val TYPE_EARTH =0
 const val TYPE_MARS =1
+const val TYPE_HEADER =3
 data class Data(val someText: String = "Text", val someDescription: String? = "Description",val type:Int=TYPE_MARS)
